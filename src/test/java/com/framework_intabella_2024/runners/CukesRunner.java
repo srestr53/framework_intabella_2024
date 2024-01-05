88da85ad3be09626d106f27e96a45ab1cf7eb143
srestr53 <sangil9091@gmail.com>
2024-01-04 19:09:25 -0800

    CukesRunner

---
+++ src/test/java/com/framework_intabella_2024/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.framework_intabella_2024.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/framework_intabella_2024/step_definitions",
+        dryRun = true,
+        tags = " @wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
