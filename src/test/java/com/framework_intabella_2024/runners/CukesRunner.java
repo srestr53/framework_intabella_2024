c2dec0a5b66cec4a4c89cbc01d4b4272690ee134
srestr53 <sangil9091@gmail.com>
2024-01-07 07:17:47 -0800

    CukesRunner  update

--- src/test/java/com/framework_intabella_2024/runners/CukesRunner.java
+++ src/test/java/com/framework_intabella_2024/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/framework_intabella_2024/step_definitions",
-        dryRun = true,
-        tags = " @wip",
+        dryRun = false,
+        tags = "@wip",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
