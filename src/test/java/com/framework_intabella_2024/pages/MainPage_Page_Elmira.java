dd2d2d23cae39796b6be2d1c32365130fc930e34
Elmira Utegilisheva <utegilishevaelmira@gmail.com>
2024-01-13 14:39:43 -0600

    Main page Elmira

---
+++ src/test/java/com/framework_intabella_2024/pages/MainPage_Page_Elmira.java
@@ -0,0 +1,15 @@
+package com.framework_intabella_2024.pages;
+
+import com.framework_intabella_2024.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class MainPage_Page_Elmira extends BasePage{
+
+    public MainPage_Page_Elmira() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+}
