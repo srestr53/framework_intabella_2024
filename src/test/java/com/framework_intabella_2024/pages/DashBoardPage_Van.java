3993575f09b16ec565b5e862c5e1ebb1f2341a2a
vandalat <140336926+vandalat@users.noreply.github.com>
2024-01-14 08:00:32 -0600

    update DashBoardPage_Van

--- src/test/java/com/framework_intabella_2024/pages/DashBoardPage_Van.java
+++ src/test/java/com/framework_intabella_2024/pages/DashBoardPage_Van.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class DashBoardPage_Van {
     public DashBoardPage_Van(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -14,9 +16,24 @@ public class DashBoardPage_Van {
     @FindBy(xpath = "//*[.='Calendar Events']")
     public WebElement calenderEventButton;
 
+    @FindBy(css = "[data-dismiss='alert']")
+    public WebElement popupButton;
+    @FindBy(xpath = "//*[text() = 'Vehicles']")
+    public WebElement vehiclesOption;
+
+    @FindBy(xpath = "(//a[@href='#'])[4]")
+    public WebElement fleet;
+    @FindBy(xpath = "//*[@id=\"history-content\"]/ul/li[1]/a")
+    public WebElement vehiclePageLink;
 
 
-    
-    
 
 }
+
+
+
+
+    
+    
+
+
