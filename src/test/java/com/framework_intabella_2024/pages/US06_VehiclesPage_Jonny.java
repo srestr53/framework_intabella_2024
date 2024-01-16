b27f56df4621c5d4e907c0783dde3f5589f839ee
jonny5891 <jonny5891@yahoo.com>
2024-01-15 21:13:59 -0500

    updated 1/11 5:45pm

---
+++ src/test/java/com/framework_intabella_2024/pages/US06_VehiclesPage_Jonny.java
@@ -0,0 +1,27 @@
+package com.framework_intabella_2024.pages;
+
+import com.framework_intabella_2024.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class US06_VehiclesPage_Jonny extends BasePage {
+    public US06_VehiclesPage_Jonny() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+        }
+
+        @FindBy(css = ".more-bar-holder")
+        public List<WebElement> threeDotOptions;
+        @FindBy(css = ".fa-eye.hide-text")
+        public WebElement view;
+        @FindBy(css = ".fa-eye.hide-text")
+        public WebElement edit;
+        @FindBy(css = ".fa-eye.hide-text")
+        public WebElement delete;
+
+
+    }
---
+++ src/test/java/com/framework_intabella_2024/step_definitions/US06_StepDefinitions.java
@@ -0,0 +1,42 @@
+package com.framework_intabella_2024.step_definitions;
+
+import com.framework_intabella_2024.pages.US06_DashboardPageJonny;
+import com.framework_intabella_2024.pages.US06_VehiclesPage_Jonny;
+import com.framework_intabella_2024.utilities.BrowserUtils;
+import com.framework_intabella_2024.utilities.Driver;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class US06_StepDefinitions {
+Actions actions = new Actions(Driver.getDriver());
+US06_DashboardPageJonny dashboard = new US06_DashboardPageJonny();
+    US06_VehiclesPage_Jonny vehiclePage=new US06_VehiclesPage_Jonny();
+    @When("User is on vehicles page and hovers over the three dots")
+    public void user_is_on_vehicles_page_and_hovers_over_the_three_dots() {
+        BrowserUtils.waitFor(10);
+        vehiclePage.fleet.click();
+        vehiclePage.vehicles.click();
+        for (int i = 0; i < vehiclePage.threeDotOptions.size(); i++) {
+         actions.moveToElement(vehiclePage.threeDotOptions.get(i)).pause(3).perform();
+        }
+
+
+    }
+    @Then("User sees view, edit, and delete options")
+    public void user_sees_view_edit_and_delete_options() {
+        BrowserUtils.waitFor(3);
+        Assert.assertTrue(vehiclePage.view.isEnabled());
+        Assert.assertTrue(vehiclePage.edit.isEnabled());
+        Assert.assertTrue(vehiclePage.delete.isEnabled());
+
+
+
+
+    }
+}
